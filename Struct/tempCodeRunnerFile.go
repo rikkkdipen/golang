f79caf1414f3d6c9db0a7ff6c8cb8144a9b696d4
Dipen <dipenrikkaame@gmail.com>
2024-10-22 02:54:49 +0530

    Struct example added

---
+++ Struct/tempCodeRunnerFile.go
@@ -0,0 +1,7 @@
+
+	// fmt.Println("Dipen person:", dipen) // by default stored is 0
+
+	// dipen.Firstname = "Dipen"
+	// dipen.Lastname = "Rikame"
+	// dipen.Age = 24
+	// fmt.Println("Dipen person:", dipen)
